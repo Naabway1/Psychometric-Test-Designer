fb52e1ca243f9a39a73242060ba8e4d42f383c40
nabWi <123865885+Naabway1@users.noreply.github.com>
2026-03-21 21:31:04 +0500

    SQL tables update

--- tables.sql
+++ tables.sql
@@ -1,7 +1,7 @@
 -- Таблица групп
 CREATE TABLE groups (
 	group_id VARCHAR(4) PRIMARY KEY,
-	specialization VARCHAR(200) UNIQUE
+	specialization VARCHAR(200)
 );
 
 -- Таблица пользователей
@@ -9,7 +9,7 @@ CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     login VARCHAR(100) UNIQUE,
     password TEXT,
-    group VARCHAR(4) REFERENCES groups(group_id) ON DELETE CASCADE,
+    group_id VARCHAR(4) REFERENCES groups(group_id) ON DELETE CASCADE,
     created_at TIMESTAMP DEFAULT NOW()
 );
 
@@ -20,29 +20,6 @@ CREATE TABLE metrics (
     description TEXT
 );
 
--- Метрики каждого пользователя
-CREATE TABLE user_metrics (
-    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
-    metric_id INT REFERENCES metrics(metric_id) ON DELETE CASCADE,
-    value NUMERIC DEFAULT 0,
-    PRIMARY KEY(user_id, metric_id)
-);
-
--- Таблица тестов
-CREATE TABLE tests (
-    test_id SERIAL PRIMARY KEY,
-    title VARCHAR(255),
-    created_by INT REFERENCES users(user_id),
-    created_at TIMESTAMP DEFAULT NOW()
-);
-
--- Таблица вопросов
-CREATE TABLE questions (
-    question_id SERIAL PRIMARY KEY,
-    test_id INT REFERENCES tests(test_id) ON DELETE CASCADE,
-    text TEXT - текст самого вопроса
-);
-
 -- Таблица шкал
 CREATE TABLE scales (
     scale_id SERIAL PRIMARY KEY,
@@ -50,34 +27,63 @@ CREATE TABLE scales (
     description TEXT
 );
 
+-- Метрики каждого пользователя
+CREATE TABLE user_metrics (
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    metric_id INT REFERENCES metrics(metric_id) ON DELETE CASCADE,
+    user_metric_value NUMERIC NOT NULL DEFAULT 0,
+    PRIMARY KEY(user_id, metric_id)
+);
+
+-- Таблица тестов
+CREATE TABLE tests (
+    test_id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    created_by INT REFERENCES users(user_id) SET NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+-- Таблица вопросов
+CREATE TABLE questions (
+    question_id SERIAL PRIMARY KEY,
+    test_id INT REFERENCES tests(test_id) ON DELETE CASCADE,
+    text TEXT NOT NULL
+);
+
 -- Таблица ответов
 CREATE TABLE answers (
     answer_id SERIAL PRIMARY KEY,
     question_id INT REFERENCES questions(question_id) ON DELETE CASCADE,
-    text TEXT - текст самого ответа
+    text TEXT NOT NULL,
+    answer_value NUMERIC NOT NULL
 );
 
--- Таблица ответов пользователей
-CREATE TABLE user_answers (
+-- Связь: вопрос -> шкала
+CREATE TABLE question_scale (
     question_id INT REFERENCES questions(question_id) ON DELETE CASCADE,
-    answer_id INT REFERENCES answers(answer_id) ON DELETE CASCADE,
-    PRIMARY KEY(attempt_id, question_id)
-);
-
--- Влияние ответа на шкалу
-CREATE TABLE answer_scale_effects (
-    answer_id INT REFERENCES answers(answer_id) ON DELETE CASCADE,
     scale_id INT REFERENCES scales(scale_id) ON DELETE CASCADE,
-    effect_value NUMERIC,  -- положительное или отрицательное влияние
-    PRIMARY KEY(answer_id, scale_id)
+    weight NUMERIC NOT NULL,
+    PRIMARY KEY(question_id, scale_id)
 );
 
--- Связь шкал с метриками
-CREATE TABLE scale_metric_mapping (
+-- Связь: шкала -> метрика (в контексте теста)
+CREATE TABLE test_scale_metric (
+    test_id INT REFERENCES tests(test_id) ON DELETE CASCADE,
     scale_id INT REFERENCES scales(scale_id) ON DELETE CASCADE,
     metric_id INT REFERENCES metrics(metric_id) ON DELETE CASCADE,
-    weight NUMERIC, -- коэффициент преобразования шкалы в метрику
-    PRIMARY KEY(scale_id, metric_id)
+    weight NUMERIC NOT NULL, -- сумма по (test_id, metric_id) = 1 (и только лишь 1)
+    PRIMARY KEY(test_id, scale_id, metric_id)
+);
+
+-- Таблица результат шкал 
+CREATE TABLE user_scale_results (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    scale_id INT REFERENCES scales(scale_id) ON DELETE CASCADE,
+    raw_score NUMERIC,
+    normalized_score NUMERIC,
+    source_test_id INT REFERENCES tests(test_id),
+    created_at TIMESTAMP DEFAULT NOW()
 );
 
 -- Снапшоты метрик пользователей (раз в N дней)
@@ -85,7 +91,7 @@ CREATE TABLE user_metric_snapshots (
     id SERIAL PRIMARY KEY,
     user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
     metric_id INT REFERENCES metrics(metric_id) ON DELETE CASCADE,
-    value NUMERIC,
+    ums_value NUMERIC,
     source_test_id INT REFERENCES tests(test_id),
     created_at TIMESTAMP DEFAULT NOW()
 );
